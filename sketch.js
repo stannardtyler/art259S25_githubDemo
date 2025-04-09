0cf91370a216e89a6b38e534d18cc048325988da
stannardtyler <tstannard64@gmail.com>
2025-04-08 17:17:14 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,9 +2,11 @@ function setup() {
   let canvas = createCanvas(400, 400);
   canvas.parent("thegame");
   
-  background(220);
+  background(0);
 }
 
 function draw() {
+  fill(0);
+  stroke(255);
   ellipse(mouseX, mouseY, 20);
 }
